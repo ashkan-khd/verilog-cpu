860a1d33fa36f2fad057e46f9f68c54c536eca0f
Ashkan Khademian <ashkan.khd.q@gmail.com>
2021-06-01 23:22:15 +0430

    fix bugs

--- control_unit.v
+++ control_unit.v
@@ -1,18 +1,39 @@
 module ControlUnit (
-
+    clk, 
+    init_PC, 
+    alu_out, 
+    stack_out, 
+    flags, 
+    cache_a_b_not, 
+    is_data_indirect, 
+    ALUOP, 
+    pop_operand, 
+    push_result, 
+    write_mem_result,
+    datapath_in,
+    data_memory_write_addr
 );
-`define START 3'b001
-`define MID 3'b010
-`define END 3'b100
+
 parameter DATA_RANGE = 8;
 parameter INST_RANGE = 12;
 parameter OP_CODE_RANGE = 4;
+parameter FLAGS_COUNT = 2;
+localparam START = 3'b001;
+localparam MID = 3'b010;
+localparam END = 3'b100;
+
 
 input clk;
 input [DATA_RANGE-1:0] init_PC;
+input [DATA_RANGE-1:0] alu_out, stack_out;
+input [FLAGS_COUNT-1:0] flags;
+output reg cache_a_b_not, is_data_indirect, ALUOP, pop_operand, push_result, write_mem_result;
+output reg [DATA_RANGE-1:0] datapath_in;
+output reg [DATA_RANGE-1:0] data_memory_write_addr;
 
 reg [DATA_RANGE-1:0] PC;
 reg [INST_RANGE-1:0] IR;
+
 
 Memory instruction_memory(
     PC,
@@ -24,7 +45,7 @@ defparam instruction_memory.N = DATA_RANGE;
 defparam instruction_memory.M = INST_RANGE;
 
 wire [OP_CODE_RANGE-1:0] op_code;
-assign op_code = IR[INST_RANGE-1:INST_RANGE-OP_CODE_RANGE]
+assign op_code = IR[INST_RANGE-1:INST_RANGE-OP_CODE_RANGE];
 reg [3:1] states;
 
 initial begin
@@ -32,21 +53,98 @@ initial begin
     states = 3'b000;
 end
 
-always @(clk) begin
-    if (states==START) begin
+always @(posedge clk) begin
+    if (states == START) begin
+		/* cache_a_b_not = 0;
+		is_data_indirect = 0;
+		ALUOP = 0;
+		pop_operand = 0;
+		push_result = 0;
+		write_mem_result = 0; */ 
+		push_result = 0;
+		write_mem_result = 0;
+		
         case (op_code)
-            4'b0000: states=
-            default: 
+            4'b0000: begin
+				cache_a_b_not = 0;
+				datapath_in = 0;
+				cache_a_b_not = 1;
+				datapath_in = IR[DATA_RANGE-1:0];
+				push_result = 1;
+				PC = PC + 1;
+            end
+            4'b0001: begin
+				cache_a_b_not = 0;
+				datapath_in = 0;
+				cache_a_b_not = 1;
+				is_data_indirect = 1;
+				datapath_in = IR[DATA_RANGE-1:0];
+				push_result = 1;
+				PC = PC + 1;
+            end
+			4'b0110, 4'b0111, 4'b0010, 4'b0011: begin
+				pop_operand = 1;
+				states = MID;
+            end
+			4'b0100: begin
+				if(flags[0]) begin
+					pop_operand = 1;
+					states = MID;
+				end else begin
+					PC = PC + 1;
+				end 
+            end
+			4'b0101: begin
+				if(flags[1]) begin
+					pop_operand = 1;
+					states = MID;
+				end else begin
+					PC = PC + 1;
+				end 
+            end
         endcase
     end
 end
 
+always @(posedge clk)
+	if(states == MID) begin 
+		push_result = 0;
+		write_mem_result = 0;
+		case(op_code)
+			4'b0010: begin
+				cache_a_b_not = 1;
+				datapath_in = stack_out;
+				cache_a_b_not = 0;
+				datapath_in = 0;
+				pop_operand = 0;
+				write_mem_result = 1;
+				data_memory_write_addr = IR[DATA_RANGE-1:0];
+				write_mem_result = 0;
+				PC = PC + 1;
+				states = START;
+            end
+			4'b0100, 4'b0101, 4'b0011: begin
+				pop_operand = 0;
+				PC = stack_out;
+				states = START;
+            end
+			4'b0110, 4'b0111: begin
+                cache_a_b_not = 1;
+				datapath_in = stack_out;
+				cache_a_b_not = 0;
+				states = END;
+            end
+		endcase
+	end
 
-function [3:1] states_after_push_constance();
-    begin
-        
-    end
-endfunction
-
+always @(posedge clk)
+	if(states == END) begin
+        datapath_in = stack_out;
+        pop_operand = 0;
+        push_result = 1;
+        ALUOP = op_code[0];
+        states = START;
+        PC = PC + 1;
+	end
     
 endmodule
--- data_path.v
+++ data_path.v
@@ -1,4 +1,17 @@
 module DataPath (
+    clk,
+    data_in,
+    cache_a_b_not, 
+    is_data_indirect, 
+    ALUOP, 
+    pop_operand,
+    push_result, 
+    write_mem_result,
+    write_address,
+    data_out,
+    datapath_alu_out, 
+    datapath_stack_out,
+    flags
 );
 parameter WORD_RANGE = 8;
 parameter MEMORY_WORD_COUNT = 256;
@@ -11,28 +24,36 @@ input push_result, write_mem_result;
 input [WORD_RANGE-1:0] write_address;
 input clk;
 output [WORD_RANGE-1:0] data_out;
-
-
+output [WORD_RANGE-1:0] datapath_alu_out, datapath_stack_out;
 output [FLAGS_COUNT-1:0] flags;
 
-assign data_out = pop_operand ? stack_out : alu_out;
 
+// alu segment
+reg [WORD_RANGE-1:0] cache_a, cache_b;
+reg alu_cin;
+wire [WORD_RANGE-1:0] alu_out;
 
-always @(*) begin
-    if (cache_a_b_not) begin
-        if (is_data_indirect) begin
-            cache_a = data_memory_bus;
-        end else begin
-            cache_a = data_in;
-        end
-    end else begin
-        if (is_data_indirect) begin
-            cache_b = data_memory_bus;
-        end else begin
-            cache_b = data_in;
-        end
-    end
+wire cout, zero;
+ALU alu(
+    cache_a,
+    cache_b,
+    alu_cin,
+    ALUOP,
+    alu_out,
+    cout,
+    zero
+);
+defparam alu.TERMINAL_RANGE = WORD_RANGE;
+
+assign flags[0] = zero;
+assign flags[1] = alu_out[WORD_RANGE-1];
+
+initial begin
+    alu_cin = 0;
+    cache_a = {WORD_RANGE{1'b0}};
+    cache_b = {WORD_RANGE{1'b0}};
 end
+
 
 // stack segment
 reg reset_stack;
@@ -52,33 +73,8 @@ initial begin
 end
 
 
-// alu segment
-reg [WORD_RANGE-1:0] cache_a, cache_b;
-reg alu_cin;
-wire [WORD_RANGE-1:0] alu_out;
-wire cout, zero;
-ALU alu(
-    cache_a,
-    cache_b,
-    alu_cin,
-    ALUOP,
-    alu_out,
-    cout,
-    zero
-);
-alu.TERMINAL_RANGE = WORD_RANGE;
-
-assign flags[0] = zero;
-assign flags[1] = alu_out[WORD_RANGE-1];
-
-initial begin
-    alu_cin = 0;
-    cache_a = {WORD_RANGE{1'b0}};
-    cache_b = {WORD_RANGE{1'b0}};
-end
-
 // data memory segment
-wire [WORD_RANGE-1] data_memory_address;
+wire [WORD_RANGE-1:0] data_memory_address;
 MUX mux(
     write_mem_result,
     write_address,
@@ -87,7 +83,7 @@ MUX mux(
 );
 defparam mux.WORD_RANGE = WORD_RANGE;
 
-wire [WORD_RANGE-1] data_memory_bus;
+wire [WORD_RANGE-1:0] data_memory_bus;
 Memory data_memory(
     data_memory_address,
     write_mem_result, 
@@ -104,5 +100,28 @@ TriStateBuffer tri_state_buffer(
 );
 defparam tri_state_buffer.RANGE = WORD_RANGE;
 
+
+// input output assignments
+assign datapath_alu_out = alu_out; 
+assign datapath_stack_out = stack_out;
+
+assign data_out = pop_operand ? stack_out : alu_out;
+
+
+always @(*) begin
+    if (cache_a_b_not) begin
+        if (is_data_indirect) begin
+            cache_a = data_memory_bus;
+        end else begin
+            cache_a = data_in;
+        end
+    end else begin
+        if (is_data_indirect) begin
+            cache_b = data_memory_bus;
+        end else begin
+            cache_b = data_in;
+        end
+    end
+end
 
 endmodule
